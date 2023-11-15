66c66d4641a629e5f460a6f4a40bc98da7a4c858
Mateusz Świetlik <swiecium@gmail.com>
2023-11-15 17:52:28 +0100

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,4 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const gallery = document.querySelector(".gallery");
 
@@ -12,24 +12,29 @@ for (const image of galleryItems) {
       alt="${image.description}"
     />
   </a>
-</div>`
-  gallery.insertAdjacentHTML('beforeend', item);
-};
+</div>`;
+  gallery.insertAdjacentHTML("beforeend", item);
+}
 
 const handleClick = (event) => {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
   event.preventDefault();
   function onEscape(event) {
     if (event.code !== "Escape") return;
     instance.close();
   }
-  const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(
+    `
     <img src= ${event.target.dataset.source}>
     `,
     {
       onShow: () => window.addEventListener("keydown", onEscape),
       onClose: () => window.removeEventListener("keydown", onEscape),
-    },)
+    }
+  );
   instance.show();
-}
+};
 
-gallery.addEventListener('click', handleClick);
+gallery.addEventListener("click", handleClick);
